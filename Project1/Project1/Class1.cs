b76821872168cb10c300288da00b756ce8153f84
keewti <125391377+keewti@users.noreply.github.com>
2023-02-15 21:01:46 +0300

    initial commit

---
+++ Project1/Project1/Class1.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter M");
+double m = Convert.ToDouble(Console.ReadLine());
+var rnd = new Random();
+double probability = rnd.NextDouble();
+if (probability >= m)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int number = rnd.Next(0, 1000);
+        Console.Write($"{number} ");
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Sum of all digits of this random numbers is {sum}");
+}
+else
+{
+    for (int i = 0; i < n; i++)
+    {
+        int number = rnd.Next(0, 1000);
+        Console.Write($"{number} ");
+        while (number > 0)
+        {
+            Console.Write(number % 10);
+            number /= 10;
+        }
+        Console.WriteLine();
+    }
+}
