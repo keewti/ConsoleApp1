2f2a9cb0fd2f61099edb43daeb1d700e6ec406ae
keewti <125391377+keewti@users.noreply.github.com>
2023-02-20 15:43:13 +0300

    bad lambda integration

--- Project1/ConsoleApp1/Program.cs
+++ Project1/ConsoleApp1/Program.cs
@@ -1,41 +1,46 @@
-﻿int GeneratingRandomNumber(ref int a)
+﻿namespace Project1
 {
-    int b = a % 10;
-    a /= 10;
-    return (b);
-}
-
-Console.WriteLine("Enter N");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter M");
-double m = Convert.ToDouble(Console.ReadLine());
-var rnd = new Random();
-double probability = rnd.NextDouble();
-if (probability >= m)
-{
-    int sum = 0;
-    for (int i = 0; i < n; i++)
+    public static class Program
     {
-        int number = rnd.Next(0,1000);
-        Console.Write($"{number} ");
-        while (number > 0)
+        public static void Main(string[] args)
         {
-            sum += GeneratingRandomNumber(ref number);
+            Func<int, int> DoingSomething = a => a % 10;  
+            Console.WriteLine("Enter N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter M");
+            double m = Convert.ToDouble(Console.ReadLine());
+            var rnd = new Random();
+            double probability = rnd.NextDouble();
+            if (probability >= m)
+            {
+                int sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    int number = rnd.Next(0, 1000);
+                    Console.Write($"{number} ");
+                    while (number > 0)
+                    {
+                        sum += DoingSomething(number);
+                        number /= 10;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine($"Sum of all digits of this random numbers is {sum}");
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int number = rnd.Next(0, 1000);
+                    Console.Write($"{number} ");
+                    while (number > 0)
+                    {
+                        Console.Write(DoingSomething(number));
+                        number /= 10;
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
-    }
-    Console.WriteLine();
-    Console.WriteLine($"Sum of all digits of this random numbers is {sum}");
-}
-else
-{
-    for (int i = 0; i < n; i++)
-    {
-        int number = rnd.Next(0, 1000);
-        Console.Write($"{number} ");
-        while (number > 0)
-        {
-            Console.Write(GeneratingRandomNumber(ref number));
-        }
-        Console.WriteLine();
     }
 }
