f28e93bdb6d6e72320c6e00dbdf20e2fd5aad724
keewti <125391377+keewti@users.noreply.github.com>
2023-03-06 20:36:15 +0300

    command line parameters without parsing

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -9,11 +9,20 @@ namespace ConsoleApp2
     {
         public static void Main(string[] args)
         {
-
-            Console.WriteLine("Enter N");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter M");
-            double m = Convert.ToDouble(Console.ReadLine());
+            int n;
+            double m;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter N");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter M");
+                m = Convert.ToDouble(Console.ReadLine());
+            }
+            else
+            {
+                n = Convert.ToInt32(args[0]);
+                m = Convert.ToDouble(args[1]);
+            }
             var rnd = new Random();
             double probability = new Random().NextDouble();
             IEnumerable<int> numbers = Enumerable.Range(0, n).Select(_ => rnd.Next(0, 1000)).ToList();
