7469f9a9f246e6d5e769ace831ba6f8e8a840214
keewti <125391377+keewti@users.noreply.github.com>
2023-05-05 01:04:26 +0300

    trying something after break

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,30 +7,15 @@ namespace ConsoleApp2
 {
     public static class Program
     {
-
-        static void Writing(string text, string filep)
-        {
-            if (filep == " ")
-            {
-                Console.WriteLine(text);
-            }   
-            else
-            {
-                using (StreamWriter writer = new StreamWriter(filep, true))
-                {
-                    writer.WriteLine(text);
-                }
-            }
-        }
         public static void Main(string[] args)
         {
             int n = 0;
             double m = 0;
             bool isN = false, isM = false;
-            string filepath = " ";
+            string filepath = null;
             if (args.Length != 0)
             {
-                for (int i = 0; i < args.Length; i++)
+                for (int i = 0; i < args.Length - 1; i += 2)
                 {
                     switch (args[i])
                     {
@@ -58,21 +43,32 @@ namespace ConsoleApp2
                 Console.WriteLine("Enter M");
                 m = Convert.ToDouble(Console.ReadLine());
             }
+            Action<string> WriteWhereItBelongs;
+            StreamWriter writer = null;
+            if (string.IsNullOrEmpty(filepath))
+            {
+                WriteWhereItBelongs = Console.WriteLine;
+            }
+            else
+            {
+                writer = new StreamWriter(filepath, true);
+                WriteWhereItBelongs = writer.WriteLine;
+            }
             var rnd = new Random();
             double probability = new Random().NextDouble();
             IEnumerable<int> numbers = Enumerable.Range(0, n).Select(_ => rnd.Next(0, 1000)).ToList();
-            Writing("Initial numbers are:", filepath);
+            WriteWhereItBelongs("Initial numbers are:");
             foreach (var k in numbers)
             {
-                Writing($"{k} ", filepath);
+                WriteWhereItBelongs($"{k} ");
             }
             if (probability >= m)
             {
-                Writing($"Reverted numbers are", filepath);
+                WriteWhereItBelongs($"Reverted numbers are");
                 foreach (var k in numbers)
                 {
                     string b = new string (k.ToString().Reverse().ToArray());
-                    Writing($"{b} ", filepath);
+                    WriteWhereItBelongs($"{b} ");
                 }
             }
             else
@@ -86,7 +82,8 @@ namespace ConsoleApp2
                         sum += Convert.ToInt32(b.ToString());
                     }
                 }
-                Writing($"Sum of all chasrecters of this numbers are {sum}", filepath);
+                WriteWhereItBelongs($"Sum of all chasrecters of this numbers are {sum}");
+                writer?.Dispose();
             }
         }
     }
